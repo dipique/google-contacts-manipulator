9c1d31121b42fd322f1021ae7086f4ac6619e430
Daniel <dipique@gmail.com>
2017-04-04 09:57:26 -0500

    Add project files.

---
+++ Google Contacts Manipulator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Google_Contacts_Manipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
